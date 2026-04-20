b625122491603ecca3978cfc3144bb6b46df8a7a
j6lsja <jon857321@gmail.com>
2026-04-20 14:16:50 +1000

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.get("/load/:userId", (req, res) => {
     res.send(database[userId] || { exp: 0 });
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
